c3de9e15ace6c9f693b29365333d36acfb33f781
veronicavillegas <urseza@gmail.com>
2014-10-20 16:26:17 -0300

    Added threads

--- PrimosCirculares/src/primoscirculares/CircularNumberFinderRunnable.java
+++ PrimosCirculares/src/primoscirculares/CircularNumberFinderRunnable.java
@@ -12,29 +12,31 @@ import java.util.ArrayList;
  * @author Veronica
  */
 public class CircularNumberFinderRunnable implements Runnable {
-    private ArrayList<Integer> numbersList;
-    private ArrayList<Integer> possibleCircularNumbers;
 
-    public CircularNumberFinderRunnable(ArrayList<Integer> numberList) {
-        this.numbersList = numberList;
+    private ArrayList<Integer> possibleCircularNumbers = new ArrayList<>();
+    private int from;
+    private int until;
+
+    public CircularNumberFinderRunnable(int from, int until) {
+        this.from = from;
+        this.until = until;
     }
-    
+
     @Override
     public void run() {
-        numbersList = new ArrayList<>();
-        for(Integer number : numbersList){
-            if(isPossibleToBeCircular(number)){
-                numbersList.add(number);
+        for (int i = from; i < until; i++) {
+            if (isPrime(i) && isPossibleToBeCircular(i)) {
+                possibleCircularNumbers.add(i);
             }
         }
+        PrimosCirculares.addPossiblePrimeCircular(possibleCircularNumbers);
     }
-    
-    public ArrayList<Integer> getPossibleCircularNumbers() {
-        return possibleCircularNumbers;
-    }
-    
+
     private static boolean isPossibleToBeCircular(int number) {
         String stringNumber = String.valueOf(number);
+        if (number < 10) {
+            return false;
+        }
         boolean isPossibleToBeCircular = true;
         if (stringNumber.contains("0")
                 || stringNumber.contains("2")
@@ -46,5 +48,17 @@ public class CircularNumberFinderRunnable implements Runnable {
         }
         return isPossibleToBeCircular;
     }
-    
+
+    private static boolean isPrime(int n) {
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- PrimosCirculares/src/primoscirculares/PrimosCirculares.java
+++ PrimosCirculares/src/primoscirculares/PrimosCirculares.java
@@ -13,77 +13,105 @@ import java.util.ArrayList;
  */
 public class PrimosCirculares {
 
+    private static boolean[] isPrimeNumber;
+    private static ArrayList<Integer> possiblePrimeCircular = new ArrayList<>();
+
+    public static synchronized void addPossiblePrimeCircular(ArrayList<Integer> numbers) {
+        possiblePrimeCircular.addAll(numbers);
+    }
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        ArrayList<Integer> primeNumbers = getPrimeNumbersBetween(20, 100);
-        /*Hilos para obtener los posibles numeros circulares*/
-        /*int limiteHilos = 5;
+    public static void main(String[] args) throws InterruptedException {
+        GetPossibleCircularPrimes();
+    }
+
+    private static void GetPossibleCircularPrimes() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<Thread>();
-        for(int i = 0; i <= limiteHilos; i++){
-            Runnable task = new CircularNumberFinderRunnable(primeNumbers);
+        int j = 0;
+        for (int i = 0; i <= 20; i++) {
+            Runnable task = new CircularNumberFinderRunnable(j, j += 50000);
             Thread worker = new Thread(task);
             worker.setName("Task_" + i);
             worker.start();
             threads.add(worker);
-        }*/
-        
-        
-        
-        //ArrayList<Integer> possibleCircularNumbers = primeNumbers;
-        /*Hilos*/
-        //getCircularNumbers(possibleCircularNumbers);
-        getRotations(1234567);
+        }
+        boolean isThereThreadRunning = true;
+        while (isThereThreadRunning) {
+            for (Thread thread : threads) {
+                if (thread.isAlive()) {
+                    isThereThreadRunning = true;
+                    break;
+                } else {
+                    isThereThreadRunning = false;
+                }
+            }
+            if (isThereThreadRunning) {
+                Thread.sleep(10);
+            } else {
+                break;
+            }
+        }
     }
-    
-    
+
     /**
-     Metodo Criba de Eratóstenes: http://es.wikipedia.org/wiki/Criba_de_Erat%C3%B3stenes
-     */   
-    public static ArrayList<Integer> getPrimeNumbersBetween(int number) 
-    {
+     * Metodo Criba de Eratóstenes:
+     * http://es.wikipedia.org/wiki/Criba_de_Erat%C3%B3stenes
+     */
+    public static ArrayList<Integer> getPossibleCircularPrimeNumbersUnder(int number) {
         //Marco todos los números de la serie como primos
-        boolean[] isPrimeNumber = new boolean[number + 1];
-        for (int i = 2; i <= number; i++) 
-        {
+        isPrimeNumber = new boolean[number + 1];
+        for (int i = 2; i <= number; i++) {
             isPrimeNumber[i] = true;
         }
         //Tomo el primer numero como primo. 
         //Si elevado al cuadrado es mayor o igual al numero dado, el algoritmo termina,
         //sino, evaluo el arreglo buscando los múltiplos
-        for (int i = 2; i*i <= number; i++)
-        {
-            for (int j = i; i*j <= number; j++) 
-            {
-                isPrimeNumber[i*j] = false;
+        for (int i = 2; i * i <= number; i++) {
+            for (int j = i; i * j <= number; j++) {
+                isPrimeNumber[i * j] = false;
             }
         }
         ArrayList<Integer> primos = new ArrayList<>();
         for (int i = 2; i <= number; i++) {
-            if (isPrimeNumber[i])
-            {
+            if (isPrimeNumber[i]) {
                 primos.add(i);
             }
         }
         return primos;
     }
-    
-   
 
-    
-   
+    private static ArrayList<Integer> getCircularNumbers(ArrayList<Integer> possibleCircularPrimeNumbers) {
+        ArrayList<Integer> circularPrimeNumbers = new ArrayList<>();
 
-    private static void getCircularNumbers(ArrayList<Integer> possibleCircularNumbers) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (Integer number : possibleCircularPrimeNumbers) {
+            boolean isCircularNumber = false;
+
+            int[] rotations = getRotations(number);
+            for (int n : rotations) {
+                if (isPrimeNumber[n]) {
+                    isCircularNumber = true;
+                } else {
+                    isCircularNumber = false;
+                    break;
+                }
+            }
+            if (isCircularNumber) {
+                for (int n : rotations) {
+                    circularPrimeNumbers.add(n);
+                }
+            }
+        }
+        return circularPrimeNumbers;
     }
-    
-    private static int[] getRotations(int number){
+
+    private static int[] getRotations(int number) {
         String stringNumber = String.valueOf(number);
         int totalRotations = stringNumber.length();
         int[] rotations = new int[totalRotations];
         String word = stringNumber;
-        for(int i = 0; i < totalRotations; i++){
+        for (int i = 0; i < totalRotations; i++) {
             word = word.substring(1, word.length()) + word.charAt(0);
             rotations[i] = Integer.parseInt(word);
         }
